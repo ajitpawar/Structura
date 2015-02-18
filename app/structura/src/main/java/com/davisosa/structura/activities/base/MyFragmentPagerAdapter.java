d1b5e0adc2d00684680517a59c49019a13072930
Ajit Pawar <me@pawar.ca>
2015-02-18 02:51:20 -0500

    implementing FragmentAdapter

--- app/structura/src/main/java/com/davisosa/structura/activities/base/MyFragmentPagerAdapter.java
+++ app/structura/src/main/java/com/davisosa/structura/activities/base/MyFragmentPagerAdapter.java
@@ -6,4 +6,29 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+    private Context context;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
 }
+
