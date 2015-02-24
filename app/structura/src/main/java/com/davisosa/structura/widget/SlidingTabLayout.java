b4f2f54931e3944156f6be9ce4cc312128ad0899
Ajit Pawar <me@pawar.ca>
2015-02-24 17:58:01 -0500

    Revert "Revert "reorganizing app directory structure""
    This reverts commit 03aed718cb493e73cc57436b8aa44b0bf79b3c07.

--- app/structura/src/main/java/com/davisosa/structura/widget/SlidingTabLayout.java
+++ app/structura/src/main/java/com/davisosa/structura/widget/SlidingTabLayout.java
@@ -14,11 +14,10 @@
  * limitations under the License.
  */
 
-package com.davisosa.structura.util;
+package com.davisosa.structura.widget;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
