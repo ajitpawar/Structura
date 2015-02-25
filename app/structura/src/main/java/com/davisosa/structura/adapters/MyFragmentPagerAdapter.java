5605b1bc21f362c58d57c11e2855950088799d70
Ajit Pawar <me@pawar.ca>
2015-02-24 19:08:49 -0500

    externalizing tab titles to strings.xml

--- app/structura/src/main/java/com/davisosa/structura/adapters/MyFragmentPagerAdapter.java
+++ app/structura/src/main/java/com/davisosa/structura/adapters/MyFragmentPagerAdapter.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import com.davisosa.structura.R;
 import com.davisosa.structura.fragments.PageFragment;
 
 public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
-    private String tabTitles[] = new String[] { "Play", "Summary", "Quiz" };
+    //private String tabTitles[] = new String[] { "Play", "Summary", "Quiz" };
     private Context context;
 
     public MyFragmentPagerAdapter(FragmentManager fm, Context context) {
@@ -29,7 +30,16 @@ public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         // Generate title based on item position
-        return tabTitles[position];
+        switch (position){
+            case 0:
+                return context.getResources().getString(R.string.title_tab1);
+            case 1:
+                return context.getResources().getString(R.string.title_tab2);
+            case 2:
+                return context.getResources().getString(R.string.title_tab3);
+        }
+
+        return null;
     }
 }
 
