fa25282d2bd2539d0796d4e8b454892bfb74ea2b
SeanWiz <triclopszx@gmail.com>
2015-02-25 20:29:17 -0500

    Created button fragment
    	modified:   app/structura/src/main/java/com/davisosa/structura/activities/MainActivity.java
    	new file:   app/structura/src/main/java/com/davisosa/structura/fragments/InsertDeleteFragment.java
    	renamed:    app/structura/src/main/res/layout/insert_delete_buttons.xml -> app/structura/src/main/res/layout/fragment_insert_delete.xml
    	modified:   app/structura/src/main/res/values/strings.xml

--- app/structura/src/main/java/com/davisosa/structura/activities/MainActivity.java
+++ app/structura/src/main/java/com/davisosa/structura/activities/MainActivity.java
@@ -2,6 +2,12 @@ package com.davisosa.structura.activities;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.davisosa.structura.R;
 import com.davisosa.structura.activities.base.BaseActivity;
