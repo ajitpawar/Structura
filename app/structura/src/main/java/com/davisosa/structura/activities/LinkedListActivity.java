41dc0e77797c55c9d8e5129741a041606d999baf
Ajit Pawar <me@pawar.ca>
2015-02-26 13:40:25 -0500

    adding new acitivty: LinkedListActivity.java

---
+++ app/structura/src/main/java/com/davisosa/structura/activities/LinkedListActivity.java
@@ -0,0 +1,17 @@
+package com.davisosa.structura.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.davisosa.structura.R;
+
+/**
+ * Created by ajit on 2/26/2015.
+ */
+public class LinkedListActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.tab_layout);
+    }
+}
