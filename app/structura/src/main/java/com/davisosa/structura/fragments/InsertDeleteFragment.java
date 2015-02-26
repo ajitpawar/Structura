00dac938f954cd4a248e91e417551b3255510213
SeanWiz <triclopszx@gmail.com>
2015-02-25 20:39:00 -0500

    Removed default paramaters

--- app/structura/src/main/java/com/davisosa/structura/fragments/InsertDeleteFragment.java
+++ app/structura/src/main/java/com/davisosa/structura/fragments/InsertDeleteFragment.java
@@ -19,14 +19,7 @@ import com.davisosa.structura.R;
  * create an instance of this fragment.
  */
 public class InsertDeleteFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -39,11 +32,9 @@ public class InsertDeleteFragment extends Fragment {
      * @return A new instance of fragment InsertDeleteFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InsertDeleteFragment newInstance(String param1, String param2) {
+    public static InsertDeleteFragment newInstance() {
         InsertDeleteFragment fragment = new InsertDeleteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,10 +46,6 @@ public class InsertDeleteFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
