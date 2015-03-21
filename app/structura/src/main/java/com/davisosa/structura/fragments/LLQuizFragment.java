aba0e63600041ec4da5442be5a6c929458e843a1
Ajit Pawar <me@pawar.ca>
2015-03-21 16:17:04 -0400

    improved quiz logic, refactoring, still a bit hacky

--- app/structura/src/main/java/com/davisosa/structura/fragments/LLQuizFragment.java
+++ app/structura/src/main/java/com/davisosa/structura/fragments/LLQuizFragment.java
@@ -7,19 +7,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.RadioGroup;
+import android.widget.*;
 import com.davisosa.structura.R;
-
 
 public class LLQuizFragment extends Fragment {
 
     public static final String ARG_PAGE = "ARG_PAGE";
+    private int mPage;
 
     private Button btnSubmitQuiz;
-    int score,ans1,ans2, ans3;
-    int qCount = 3;
+    private int score = 0;
+    private int qCount = 3;
+
 
     public static LLQuizFragment newInstance(int quiz) {
         Bundle args = new Bundle();
@@ -39,78 +38,18 @@ public class LLQuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
+        // Inflate the layout
         FrameLayout fl = (FrameLayout) inflater.inflate(R.layout.fragment_quiz, container, false);
-        ViewGroup.LayoutParams lp = fl.getLayoutParams();
 
-        // Question 1
-        RadioGroup q1=(RadioGroup) fl.findViewById(R.id.answer1);
-        q1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                switch(checkedId) {
-                    case R.id.answer1A:
-                        ans1 = 1;
-                        break;
-                    case R.id.answer1B:
-                        ans1 = 2;
-                        break;
-                    case R.id.answer1C:
-                        ans1 = 3;
-                        break;
-                }
-            }
-        });
-
-        // Question 2
-        RadioGroup q2=(RadioGroup) fl.findViewById(R.id.answer2);
-        q2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                switch(checkedId) {
-                    case R.id.answer2A:
-                        ans2 = 1;
-                        break;
-                    case R.id.answer2B:
-                        ans2 = 2;
-                        break;
-                    case R.id.answer2C:
-                        ans2 = 3;
-                        break;
-                }
-            }
-        });
-
-        // Question 3
-        RadioGroup q3 = (RadioGroup) fl.findViewById(R.id.answer3);
-        q3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                switch(checkedId) {
-                    case R.id.answer3A:
-                        ans3 = 1;
-                        break;
-                    case R.id.answer3B:
-                        ans3 = 2;
-                        break;
-                    case R.id.answer3C:
-                        ans3 = 3;
-                        break;
-                }
-            }
-        });
+        // Attach listeners to all buttons
+        attachListeners(fl, qCount);       // DO NOT forget to change qCount
+                                           // when you add new questions
 
         // Submit Button
         btnSubmitQuiz = (Button) fl.findViewById(R.id.submit);
         btnSubmitQuiz.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                score = 0;
-                if(ans1 == 1) score++;
-                if(ans2 == 3) score++;
-                if(ans3 == 2) score++;
-
-                // display score to user
-                showScore(score);
+                showScore(score);    // display score to user
             }
         });
 
@@ -118,7 +57,34 @@ public class LLQuizFragment extends Fragment {
     }
 
 
-    // Display score alert
+    // Attach listeners to all radio buttons
+    private void attachListeners(FrameLayout fl, int count){
+
+        // index starts at 1, not 0
+        for(int i=1; i<=count; i++){
+            int qid = getResources().getIdentifier("q"+i, "id", fl.getContext().getPackageName());
+            RadioGroup rg =(RadioGroup) fl.findViewById(qid);
+            rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    checkAnswer(group, checkedId);
+                }
+            });
+        }
+    }
+
+    // Tally the score
+    private void checkAnswer(RadioGroup group, int checkedId){
+
+        RadioButton btn = (RadioButton) group.findViewById(checkedId);
+        int index = group.indexOfChild(btn) + 1;
+        String answer = btn.getTag().toString();
+
+        if(answer.equals(Integer.toString(index))){
+            score++;
+        }
+    }
+
+    // Display the score as an alert
     private void showScore(int score){
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
